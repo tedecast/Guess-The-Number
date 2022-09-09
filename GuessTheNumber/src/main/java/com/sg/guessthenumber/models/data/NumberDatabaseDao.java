d45435f5e8a2a8383e26b470be675f11280494a9
Teresa de Castro <decastro.ter@gmail.com>
2022-09-09 12:51:40 -0400

    Almost done NumberDataBaseDao

---
+++ GuessTheNumber/src/main/java/com/sg/guessthenumber/models/data/NumberDatabaseDao.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.guessthenumber.models.data;
+
+import com.sg.guessthenumber.models.Game;
+import com.sg.guessthenumber.models.Round;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Teresa
+ */
+@Repository
+public class NumberDatabaseDao implements NumberDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    public NumberDatabaseDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    
+    @Override
+    public Game addGame(Game game) {
+        final String sql = "INSERT INTO game(winningNumbers) VALUES(?);";
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+
+        this.jdbcTemplate.update((Connection conn) -> {
+
+            PreparedStatement statement = conn.prepareStatement(
+                    sql,
+                    Statement.RETURN_GENERATED_KEYS);
+            
+            statement.setString(1, game.getWinningNumbers());
+            return statement;
+            
+        }, keyHolder);
+        
+        game.setId(keyHolder.getKey().intValue());
+        
+        return game;
+    }
+
+    @Override
+    public List<Game> getAllGames() {
+        final String sql = "SELECT * from game;";
+        return this.jdbcTemplate.query(sql, new GameMapper());
+    }
+
+    @Override
+    public Game getGameByID(int gameID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Round addRound(Round round) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Round> getAllRounds() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static final class GameMapper implements RowMapper<Game> {
+
+        @Override
+        public Game mapRow(ResultSet rs, int index) throws SQLException {
+            Game game = new Game();
+            game.setId(rs.getInt("gameid"));
+            game.setWinningNumbers(rs.getString("winningnumbers"));
+            return game;
+
+        }
+    }
+
+        private static final class RoundMapper implements RowMapper<Round> {
+
+        @Override
+        public Round mapRow(ResultSet rs, int index) throws SQLException {
+            Round round = new Round();
+            round.setRoundNumber(rs.getInt("roundNumber"));
+            round.setGuess(rs.getString("guess"));
+            round.setPartial(rs.getInt("partial"));
+            round.setExact(rs.getInt("exact"));
+            return round;
+
+        }
+    }
+}
--- GuessTheNumber/src/main/java/com/sg/guessthenumber/models/data/service/GameService.java
+++ GuessTheNumber/src/main/java/com/sg/guessthenumber/models/data/service/GameService.java
@@ -5,10 +5,68 @@
  */
 package com.sg.guessthenumber.models.data.service;
 
+import com.sg.guessthenumber.models.Game;
+import com.sg.guessthenumber.models.Round;
+import com.sg.guessthenumber.models.data.NumberDao;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  *
  * @author Teresa
  */
 public class GameService {
     
+    private static final int numbers = 4;
+    private static final Random random = new Random();
+    
+    private final NumberDao numberDao;
+    
+    @Autowired
+    public GameService(NumberDao numberDao){
+        this.numberDao = numberDao;
+    }
+    
+    public String createWinningNumbers() {
+        // access randomly?
+        Set<Integer> nums = new HashSet<>();
+        String numToAdd = "";
+        while (nums.size() < numbers) {
+            Integer toAdd = random.nextInt();
+            nums.add(toAdd);
+        }
+        
+        List<Integer> numList = new ArrayList<>(nums);
+        Collections.shuffle(numList); // shuffle a list of numbers to get random?
+        for (int i = 0; i < numbers; i++){
+            numToAdd += numList.get(i);
+        }
+        return numToAdd;
+    }
+    
+    public Game begin() {
+        
+        Game game = new Game();
+        game.setWinningNumbers(this.createWinningNumbers());
+        this.numberDao.addGame(game);
+        return game;
+    }
+    
+    public Round guess(Round guess){
+        
+        List<Game> games = this.numberDao.getAllGames();
+        Round round = new Round();
+        
+        Round result = new Round();
+        int exact = 0;
+        int partial = 0;
+        
+        return result;     
+    }
 }
