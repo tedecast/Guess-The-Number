bd28ee8ce5b4349afe156c9d99121702cdfa9d40
Teresa de Castro <decastro.ter@gmail.com>
2022-10-22 19:06:47 -0400

    rounds by gameID works, just need to fix format

--- GuessingGame/js/home.js
+++ GuessingGame/js/home.js
@@ -54,3 +54,50 @@ $("#backButtonTwo").click(function (event) {
   $("#backButtonTwo").hide();
   $("#mainPage").show();
 });
+
+$("#roundsButton").click(function (event) {
+  $("#mainPage").hide();
+  $("#roundsPage").show();
+  $("#backButtonThree").show();
+});
+
+$("#enterButton").click(function (event) {
+  let roundContents = $("#roundContents");
+
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/rounds/" + $("#gameIDSearch").val(),
+    success: function (roundArray) {
+      $.each(roundArray, function (index, round) {
+        let roundID = round.roundID;
+        let guess = round.guess;
+        let result = round.result;
+        let guessTime = round.guessTime;
+
+        let row = "<tr>";
+        row += "<td>" + roundID + "</td>";
+        row += "<td>" + guess + "</td>";
+        row += "<td>" + result + "</td>";
+        row += "<td>" + guessTime + "</td>";
+        row += "</tr>";
+
+        roundContents.append(row);
+      });
+    },
+    error: function (result) {
+      $("#errorMessages").append(
+        $("<li>")
+          .attr({ class: "list-group-item list-group-item-danger" })
+          .text(result.responseJSON.message)
+      );
+    },
+  });
+});
+
+$("#backButtonThree").click(function (event) {
+  $("#roundContents").empty();
+  $("#gameIDSearch").val("");
+  $("#roundsPage").hide();
+  $("#backButtonThree").hide();
+  $("#mainPage").show();
+});
